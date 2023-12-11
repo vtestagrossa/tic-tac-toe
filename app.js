4febe03a22279c5998de6ce6325eddf78facb181
Vincent Testagrossa <lancevancevt@gmail.com>
2023-12-11 11:25:35 -0500

    gameboard now accepts selections and checks for winners

--- app.js
+++ app.js
@@ -1,28 +1,77 @@
 function GameBoard(){
     let gameBoard = [
-        ["","",""],
-        ["","",""],
-        ["","",""]
+        ["x","","x"],
+        ["x","x","x"],
+        ["o","x","x"]
     ];
     // Just an idea to check the row, column, diag, antidiag from each selection and determine a winner
-    // whenever a move is made. If the row is equal to the length of the board in that direction and 
-    // all the symbols are x or o, that symbol wins the game.
+    // whenever a move is made.
     let row = [];
     let column = [];
     let diag = [];
     let antidiag = [];
-    const getSelection = (x, y) => gameBoard[x][y];
+    const getSelection = (x, y) => gameBoard[y][x];
     const makeSelection = (x, y, selection) => {
         if (getSelection(x,y) === "" && selection === "x" || selection === "o"){
-            gameBoard[x][y] = selection;
+            gameBoard[y][x] = selection;
+            checkWinner(x, y, selection);
         }
-        else{
+        else if (getSelection(x,y) !== ""){
+            console.log("space not empty");
+        } else {
             console.log("Error: selection must be 'x' or 'o'");
         }
     }
-
-    return { getSelection, makeSelection, checkWinner };
+    const checkWinner = (x, y, selection) => {
+        // checks diags only if a diagonal win is possible from the current selection
+        if (x === y || 
+            (x === 0 && y === 2 
+            (x === 3 && y === 0 ))){
+            //check diag  
+            for (let i = 0; i < gameBoard.length; i++){
+                if (gameBoard[i][i] !== selection){
+                    break;
+                }
+                if (i === gameBoard.length - 1){
+                    console.log('winner on diag');
+                }
+            }
+            let j = 2;
+            //check anti-diag
+            for (let i = 0; i < gameBoard.length; i++){
+                if (gameBoard[j][i] !== selection){
+                    break
+                }
+                if (i === gameBoard.length - 1){
+                    console.log('winner on antidiag');
+                }
+                j--;
+            }
+        }
+        // check current row
+        for (let i = 0; i < gameBoard.length; i++){
+            if (gameBoard[y][i] !== selection){
+                break;
+            }
+            if (i === gameBoard.length - 1){
+                console.log('winner on row');
+            }
+        }
+        // check current column
+        for (let i = 0; i < gameBoard.length; i++){
+            if (gameBoard[i][x] !== selection){
+                break;
+            }
+            if (i === gameBoard.length - 1){
+                console.log('winner on column');
+            }
+        }
+    }
+    return { getSelection, makeSelection };
 }
 function Player(name, symbol){
     return { name, symbol };
 }
+
+const test = new GameBoard();
+test.makeSelection(1, 0, "x");
